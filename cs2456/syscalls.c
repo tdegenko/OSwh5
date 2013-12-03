86f21ae0cdab4f46cd8c68de1d96546f8b72d9f2
Tanner Degenkolb <t.degenkolb@rochester.edu>
2013-12-02 23:17:17 -0500

    more skelettonizing

--- cs2456/syscalls.c
+++ cs2456/syscalls.c
@@ -1,15 +1,30 @@
 #include <linux/kernel.h>
+#include <linux/list.h>
 #include <linux/mmzone.h>
+#include <linux/mm_types.h>
 #include <linux/slab.h>
 #include <asm-x86/uaccess_32.h>
 struct mem_stats{
-    long active,inactive,ref_active,ref_inactive,moved,evcted;
+    long active,inactive,ref_active,ref_inactive,moved,evicted;
 };
 asmlinkage long sys_cs2456_memstat(struct mem_stats * stat){
     struct mem_stats * stats;
     struct zone * pointer;
+    struct page * pos;
     stats=kmalloc(sizeof(struct mem_stats),GFP_USER);
+    stats->active=0;
+    stats->inactive=0;
+    stats->ref_active=0;
+    stats->ref_inactive=0;
+    stats->moved=0;
+    stats->evicted=0;
     for_each_zone(pointer){
+        list_for_each_entry(pos,&pointer->active_list,lru){
+            stats->active++;
+        }
+        list_for_each_entry(pos,&pointer->inactive_list,lru){
+            stats->inactive++;
+        }
     }
     if(copy_to_user(stat,stats,sizeof(struct mem_stats))!=0){
         return 1;
